64048a9b1fbd04b14a1274f876f2271e2702a186
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2026-04-15 10:40:39 +0100

    hm

--- hoodmoon/src/main.jsx
+++ hoodmoon/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { ScrollToTop } from "./hooks/scrollToTop";
+import { SkeletonTheme } from "react-loading-skeleton";
+import { Provider } from "react-redux";
+import "react-loading-skeleton/dist/skeleton.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <SkeletonTheme baseColor="#09030b" highlightColor="#1f0c26">
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </SkeletonTheme>
+  </Provider>,
+);
